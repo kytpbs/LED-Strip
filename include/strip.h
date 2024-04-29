ee3eee4bc306d8f89762fca396231531dc2cf81c
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-04-29 05:34:33 +0300

    rewrite the entire strip class again...
    I do not feel like this is an improvement though...

--- include/strip.h
+++ include/strip.h
@@ -1,22 +1,7 @@
 #pragma once
-#include <algorithm>
-#include <ArduinoIoTCloud.h>
-
-// Log data to serial if VERBOSE is defined
-//* Info logs will print data once to the serial, while VERBOSE logs will print data repeatedly.
-// Please use INFO for important logs, and VERBOSE for debugging.
-#ifdef VERBOSE
-#define LOG(x) Serial.println(x)
-#define LOG_INFO(x) Serial.println(x)
-#else
-#define LOG(x)
-#endif
-#ifdef INFO
-#define LOG_INFO(x) Serial.println(x)
-#else
-#define LOG_INFO(x)
-#warning "INFO is not defined, this will cause some logs to not be printed"
-#endif
+#include <list>
+#include <Color.h>
+#include "LEDCommand.h"
 
 enum Modes {
   Normal,
@@ -25,109 +10,55 @@ enum Modes {
   Off
 };
 
-namespace arrayFunctions {
-  /**
-   * @brief turns (0 - maxValue) into (maxValue - 0)
-   * @warning if the value is bigger than maxValue, it will be negative.
-   * If it is negative, it will be bigger than maxValue
-  */
-  void reverseValues(int* array, int arraySize, int maxValue);
-
-  void reverseValues(int* array, int arraySize);
-
-  void getDifference(int* array1, int* array2, int* result, int arraySize);
-
-  int* getDifference(int* array1, int* array2, int arraySize);
-}
 
 class LedStrip {
-    public:
-        LedStrip(uint8_t redPin, uint8_t greenPin, uint8_t bluePin, uint8_t whitePin);
-        uint32_t Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t white);
-        uint32_t Color(uint8_t red, uint8_t green, uint8_t blue);
-        uint32_t Color(CloudColoredLight* color);
-        /**
-         * @brief Update led strip, must be called in loop repeatedly.
-         * @note Rainbow and Breathe effects will not work if this is not called in loop.
-         * Normal mode will work without calling this in loop.
-        */
-        void update();
-        /**
-         * @brief Set night mode, this will turn off the led strip until night mode is turned off.
-         * @param active true if night mode is active, false otherwise
-        */
-        void setNightMode(bool active);
-        /**
-         * @brief Change color of led strip
-         * @param color color to change to
-         * @note This will work if update() is not called in loop,
-        */
-        void changeLedColor(uint32_t color);
-        /**
-         * @brief Change color of led strip
-         * @param color color to change to
-         * @note This will work if update() is not called in loop,
-         * and the function itself shouldn't be called in loop.
-        */
-        void changeLedColor(CloudColoredLight* color);
-        /**
-         * @brief Change mode of led strip
-         * @param mode mode to change to
-         * @note This will not work if update() or this function is not called in loop
-        */
-        void changeMode(Modes mode);
-        /**
-         * @brief Do rainbow effect, must be called in loop repeatedly
-         * @note This will not work if update() or this function is not called in loop
-        */
-        void doRainbow();
-        /**
-         * @brief Do rainbow effect, must be called in loop repeatedly
-        */
-        void doRainbow(CloudColoredLight* color);
-        /**
-         * @brief Do rainbow effect, must be called in loop repeatedly
-         * @param delayTime delay time in milliseconds
-        */
-        void doRainbow(CloudColoredLight* color, unsigned long delayTime);
-        /**
-         * @brief Do breathe effect, must be called in loop repeatedly
-         * @note This will not work if update() or this function is not called in loop
-        */
-        void breathe();
-        /**
-         * @brief Do breathe effect, must be called in loop repeatedly
-         * @param delayTime delay time in milliseconds
-        */
-        void breathe(unsigned long delayTime);
-
     private:
         Modes currentMode;
         Modes lastMode; // used for night mode
         bool nightModeActive;
-        uint rainbowHue;
-        uint32_t color;
+        uint16_t rainbowHue; // can't do uint8_t because it will overflow
+        SimpleColor currentColor;
 
         uint8_t redPin;
         uint8_t greenPin;
         uint8_t bluePin;
         uint8_t whitePin;
 
-        void SetupLedPins();
-        void smoothChangeTo(uint32_t color);
-        void smoothChangeTo(uint32_t color, unsigned long delayTime);
-        void smoothChangeTo(uint8_t red, uint8_t green, uint8_t blue, uint8_t white);
-        void smoothChangeTo(uint8_t red, uint8_t green, uint8_t blue, uint8_t white, unsigned long delayTime);
-        void smoothChangeTo(int* newColors, int* oldColors, unsigned long delayTime);
-        int* getAllDifferences(uint32_t color);
-        int* getAllDifferences(uint8_t red, uint8_t green, uint8_t blue, uint8_t white);
+        std::list<LEDCommand> callList;
+
+        void smoothChangeTo(SimpleColor color, unsigned long delayTime);
+        void smoothChangeTo(SimpleColor color);
+
+        void switchToRainbow();
+        void switchToBreathe();
+
+        void addToCallList(LEDCommand command);
+        void changeCallListTo(LEDCommand command);
+    public:
+        LedStrip(uint8_t redPin, uint8_t greenPin, uint8_t bluePin, uint8_t whitePin);
         /**
-         * @brief Get the All Differences object
-         * @param red red color value
-         * @param green green color value
-         * @param blue blue color value
-         * @param white white color value
-         * @param differences array of differences (array size must be 4 or greater)
+         * @brief Update led strip, must be called in loop repeatedly.
+         * @warning Nothing will work if this function is not called in loop
         */
-        void getAllDifferences(uint8_t red, uint8_t green, uint8_t blue, uint8_t white, int* differences);
+        void update();
+
+        /**
+         * @brief Set night mode, this will turn off the led strip until night mode is turned off.
+         * @param active true if night mode is active, false otherwise
+        */
+        void setNightMode(bool active);
+
+        /**
+         * @brief Change color of led strip
+         * @param color color to change to
+         * @note This will not work if update() or this function is not called in loop
+        */
+        void fillColor(SimpleColor color);
+
+        /**
+         * @brief Change mode of led strip
+         * @param mode mode to change to
+         * @note This will not work if update() or this function is not called in loop
+        */
+        void changeModeTo(Modes mode);
 };
--- src/strip.cpp
+++ src/strip.cpp
@@ -1,255 +1,138 @@
 #include "strip.h"
 
-void arrayFunctions::reverseValues(int* array, int arraySize, int maxValue) {
-    for (int i = 0; i < arraySize; i++) {
-        array[i] = maxValue - array[i];
-    }
-}
-
-void arrayFunctions::reverseValues(int* array, int arraySize) {
-    reverseValues(array, arraySize, 255);
-}
-
-void arrayFunctions::getDifference(int* array1, int* array2, int* result, int arraySize) {
-    for (int i = 0; i < arraySize; i++) {
-        result[i] = array1[i] - array2[i];
-    }
-}
-
-int* arrayFunctions::getDifference(int* array1, int* array2, int arraySize) {
-    int* result = new int[arraySize];
-    getDifference(array1, array2, result, arraySize);
-    return result;
-}
-
-// we create this object so that we can use it in the rainbow function incase the user doesn't pass in a color object
-CloudColoredLight* lastUsedColorObject = new CloudColoredLight();
 /* PUBLIC FUNCTIONS */
 LedStrip::LedStrip(uint8_t redPin, uint8_t greenPin, uint8_t bluePin, uint8_t whitePin) {
-    this->redPin = redPin;
-    this->greenPin = greenPin;
-    this->bluePin = bluePin;
-    this->whitePin = whitePin;
-    LOG_INFO("Initializing LedStrip");
-    SetupLedPins();
-}
-
-uint32_t LedStrip::Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t white) {
-    LOG("Creating color: " + String(red) + ", " + String(green) + ", " + String(blue) + ", " + String(white));
-    return (uint32_t)red << 24 | (uint32_t)green << 16 | (uint32_t)blue << 8 | (uint32_t)white;
-}
-
-uint32_t LedStrip::Color(uint8_t red, uint8_t green, uint8_t blue) {
-    return Color(red, green, blue, 0);
-}
-
-uint32_t LedStrip::Color(CloudColoredLight* color) {
-    lastUsedColorObject = color;
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-    uint8_t white;
-    
-    // calculate white using RBG, Saturation and Brightness.
-    // Use the opposite of saturation, as 0 is white and 100 is color.
-    white = ((100 - color->getSaturation()) / 100.0) * ((color->getBrightness()) / 100.0) * 255;
-    color->getCloudValue().getRGB(red, green, blue);
-
-    return Color(red, green, blue, white);
-}
-
-void LedStrip::update() {
-    switch (currentMode) {
-    case Modes::Normal:
-        // do nothing, normal mode is handled by the onColorChange() function
-        break;
-    case Modes::Off:
-        // do nothing, off mode is handled by the onModeChange() function
-        break;
-    case Modes::Rainbow:
-        doRainbow();
-        break;
-    case Modes::Breathe:
-        breathe();
-        break;
-    }
-}
-
-void LedStrip::SetupLedPins() {
     pinMode(redPin, OUTPUT);
     pinMode(greenPin, OUTPUT);
     pinMode(bluePin, OUTPUT);
     pinMode(whitePin, OUTPUT);
+
+    this->redPin = redPin;
+    this->greenPin = greenPin;
+    this->bluePin = bluePin;
+    this->whitePin = whitePin;
 }
 
-void LedStrip::changeLedColor(uint32_t color) {
-    smoothChangeTo(color); // call before setting new values, so that smooth transition can be made.
-    this->color = color;
-}
 
-void LedStrip::changeLedColor(CloudColoredLight* color) {
-    lastUsedColorObject = color;
-    uint32_t color32 = Color(color);
-    if (!color->getSwitch()) {
-        color32 = Color(0,0,0,0);
+void LedStrip::update() {
+    if (this->callList.empty()) {
+        return;
     }
-    changeLedColor(color32);
+
+    auto command = this->callList.front();
+    command.execute();
+    if (command.hasEnded()) {
+        this->callList.erase(this->callList.begin());
+    }
 }
 
-void LedStrip::changeMode(Modes mode) {
+void LedStrip::addToCallList(LEDCommand command) {
+    this->callList.push_back(command);
+}
+
+void LedStrip::changeCallListTo(LEDCommand command) {
+    this->callList.clear();
+    this->callList.push_back(command);
+}
+
+void LedStrip::fillColor(SimpleColor color) {
+    smoothChangeTo(color);
+    this->currentColor = color;
+}
+
+void LedStrip::changeModeTo(Modes mode) {
     currentMode = mode;
     switch (mode) {
     case Modes::Normal:
-        Serial.println("Changing mode to Normal");
-        changeLedColor(this->color);
-        lastUsedColorObject->setSwitch(true);
+        fillColor(this->currentColor);
         break;
     case Modes::Off:
-        Serial.println("Changing mode to Off");
-        changeLedColor(this->Color(0,0,0));
-        lastUsedColorObject->setSwitch(false);
+        fillColor(SimpleColor());
+        break;
+    case Modes::Breathe:
+        switchToRainbow();
+        break;
+    case Modes::Rainbow:
+        //TODO: implement this
         break;
     }
-}
-
-void LedStrip::doRainbow() {
-    doRainbow(lastUsedColorObject);
-}
-
-void LedStrip::doRainbow(CloudColoredLight* color) {
-    doRainbow(color, 1000);
-}
-
-void LedStrip::doRainbow(CloudColoredLight* color, unsigned long delayTime) {
-    if (currentMode != Modes::Rainbow) {
-        Serial.println("Changing mode to Rainbow");
-        changeMode(Modes::Rainbow);
-    }
-
-    rainbowHue = ((uint)color->getHue() + 8) % 360;
-
-    color->setHue(rainbowHue);
-
-    changeLedColor(color);
-}
-
-void LedStrip::breathe() {
-    breathe(1000);
-}
-
-void LedStrip::breathe(unsigned long delayTime) {
-    // We have to use smoothChangeTo instead of changeLedColor, because we want to use a different delayTime
-    uint32_t oldColor = this->color;
-    smoothChangeTo(0,0,0,0, delayTime); // turn off
-    this->color = Color(0,0,0,0); // set color to 0,0,0,0 so that the smoothChangeTo function works properly
-    smoothChangeTo(oldColor, delayTime); // turn back on
-    this->color = oldColor; // set color back to original value
 }
 
 void LedStrip::setNightMode(bool active) {
     if (active) {
         Serial.println("Changing mode to Night");
         this->lastMode = this->currentMode;
-        changeMode(Modes::Off);
+        changeModeTo(Modes::Off);
     } else {
         Serial.println("Closing night mode");
-        changeMode(this->lastMode != Modes::Off ? this->lastMode : Modes::Normal); // if last mode was off, then set to normal
+        changeModeTo(this->lastMode != Modes::Off ? this->lastMode : Modes::Normal); // if last mode was off, then set to normal
     }
 }
 
 
 /* PRIVATE FUNCTIONS */
-void LedStrip::smoothChangeTo(uint32_t color) {
-    smoothChangeTo(color, 1000);
-}
+void LedStrip::smoothChangeTo(SimpleColor color, unsigned long delayTime) {
+    int differences[4] = {abs(color.red - this->currentColor.red), abs(color.green - this->currentColor.green),
+        abs(color.blue - this->currentColor.blue), abs(color.white - this->currentColor.white)};
+    int maxDifference = ColorUtils::getMaxElement(differences, 4);
 
-void LedStrip::smoothChangeTo(uint32_t color, unsigned long delayTime) {
-    uint8_t red = (color >> 24) & 0xFF;
-    uint8_t green = (color >> 16) & 0xFF;
-    uint8_t blue = (color >> 8) & 0xFF;
-    uint8_t white = color & 0xFF;
-
-    smoothChangeTo(red, green, blue, white, delayTime);
-}
-
-void LedStrip::smoothChangeTo(uint8_t red, uint8_t green, uint8_t blue, uint8_t white) {
-    smoothChangeTo(red, green, blue, white, 1000);
-}
-
-void LedStrip::smoothChangeTo(uint8_t red, uint8_t green, uint8_t blue, uint8_t white, unsigned long delayTime) {
-    int newColors[4] = {red, green, blue, white};
-
-    int oldColors[4] = {this->color >> 24 & 0xFF, this->color >> 16 & 0xFF,
-        this->color >> 8 & 0xFF, this->color & 0xFF};
-    
-    smoothChangeTo(newColors, oldColors, delayTime);
-}
-
-void LedStrip::smoothChangeTo(int* newColors, int* oldColors, unsigned long delayTime) {
-    int pins[4] = {redPin, greenPin, bluePin, whitePin};
-
-    LOG("Changing colors to: " + String(newColors[0]) + ", " + String(newColors[1]) + ", " + String(newColors[2]) + ", " + String(newColors[3]));
-    arrayFunctions::reverseValues(newColors, 4);
-    arrayFunctions::reverseValues(oldColors, 4);
-
-    int* differences = new int[4];
-    arrayFunctions::getDifference(newColors, oldColors, differences, 4);
-
-    int abs_max_difference = abs(*std::max_element(differences, differences + 4, 
-        [](int a, int b) { return abs(a) < abs(b); }));
-
-    LOG("Max difference: " + String(abs_max_difference));
-
-    if (abs_max_difference == 0) {
+    if (maxDifference <= 0) {
         return;
     }
 
-    for (int i = 0; i <= abs_max_difference; i++) {
-        for (int j = 0; j < 4; j++) {
-            int difference = differences[j];
-            int newColor = oldColors[j];
+    for (int i = 0; i <= maxDifference; i++) {
+        // change red
+        int redDifference = color.red - this->currentColor.red;
+        int redValue = redDifference > 0 ? this->currentColor.red + i : this->currentColor.red - i;
+        this->currentColor.red = this->currentColor.red + i;
+        addToCallList(LEDCommand([=](void) {
+            analogWrite(redPin, redValue);
+        },false,"smooth change red"));
 
-            if (difference < 0) {
-                newColor -= i;
-                difference += 1;
-            }
-            if (difference > 0) {
-                newColor += i;
-                difference -= 1;
-            }
-            if (difference == 0) {
-                newColor = newColors[j];
-            }
+        // change green
+        int greenDifference = color.green - this->currentColor.green;
+        int greenValue = greenDifference > 0 ? this->currentColor.green + i : this->currentColor.green - i;
+        this->currentColor.green = this->currentColor.green + i;
+        addToCallList(LEDCommand([=](void) {
+            analogWrite(greenPin, greenValue);
+        },false,"smooth change blue"));
 
-            analogWrite(pins[j], newColor);
-            differences[j] = difference;
-        }
-    delay(1000/abs_max_difference);
-  }   
+        // change blue
+        int blueDifference = color.blue - this->currentColor.blue;
+        int blueValue = blueDifference > 0 ? this->currentColor.blue + i : this->currentColor.blue - i;
+        this->currentColor.blue = this->currentColor.blue + i;
+        addToCallList(LEDCommand([=](void) {
+            analogWrite(bluePin, blueValue);
+        },false,"smooth change green"));
+
+        // change white
+        int whiteDifference = color.white - this->currentColor.white;
+        int whiteValue = whiteDifference > 0 ? this->currentColor.white + i : this->currentColor.white - i;
+        this->currentColor.white = this->currentColor.white + i;
+        addToCallList(LEDCommand([=](void) {
+            analogWrite(whitePin, whiteValue);
+        },false,"smooth change white"));
+
+        // delay
+        addToCallList(LEDCommand([=](void) {
+            delay(delayTime/maxDifference);
+        },false,"smooth change delay"));
+    }
 }
 
-/**
- * returns an array of 4 integers, which are the differences between the current color and the new color.
-*/
-int* LedStrip::getAllDifferences(uint32_t color) {
-    return getAllDifferences((color >> 24) & 0xFF, (color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF);
+void LedStrip::smoothChangeTo(SimpleColor color) {
+    smoothChangeTo(color, 1000);
 }
 
-int* LedStrip::getAllDifferences(uint8_t red, uint8_t green, uint8_t blue, uint8_t white) {
-    int* differences = new int[4];
-    getAllDifferences(red, green, blue, white, differences);
-    return differences;
-}
-
-void LedStrip::getAllDifferences(uint8_t red, uint8_t green, uint8_t blue, uint8_t white, int* differences) {
-    int currentRed = (this->color >> 24) & 0xFF;
-    int currentGreen = (this->color >> 16) & 0xFF;
-    int currentBlue = (this->color >> 8) & 0xFF;
-    int currentWhite = this->color & 0xFF;
-
-    // cast to int to return negative values
-    differences[0] = red - currentRed; 
-    differences[1] = green - currentGreen;
-    differences[2] = blue - currentBlue;
-    differences[3] = white - currentWhite;
+void LedStrip::switchToRainbow() {
+    addToCallList(LEDCommand([=](void) {
+        SimpleColor color = ColorUtils::getColorFromHSV(this->rainbowHue, 100, 100);
+        fillColor(color);
+        this->rainbowHue += 8;
+        this->rainbowHue %= 360;
+        // we call ourselves again in the end so we do not disturb the fill commands...
+        // should have fixed this in the implementation, but this is a quick fix hopefully, will fix later
+        // this is not a recursive function, but will keep get called by the update function until the mode is changed
+        this->switchToRainbow();
+    },false, "Rainbow"));
 }
