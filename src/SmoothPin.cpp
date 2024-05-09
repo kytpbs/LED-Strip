806fdf9653f15c0544e2d459f2a03dcc07f21866
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-05-09 07:32:47 +0300

    Create SmoothPin class

---
+++ src/SmoothPin.cpp
@@ -0,0 +1,37 @@
+#include "SmoothPin.h"
+
+SmoothPin::SmoothPin(uint8_t pin) {
+    this->pin = pin;
+    pinMode(pin, OUTPUT);
+    this->currentValue = 0;
+    this->targetValue = 0;
+    this->lastChangeTime = 0;
+    this->delayTime = 0;
+}
+
+void SmoothPin::write(int value, unsigned int totalDelayMillis) {
+    this->targetValue = value;
+    this->lastChangeTime = 0;
+    this->delayTime = totalDelayMillis / abs(this->currentValue - value);
+}
+
+void SmoothPin::write(int value) {
+    this->write(value, 0);
+}
+
+void SmoothPin::update() {
+    if (this->currentValue == this->targetValue) {
+        return;
+    }
+
+    unsigned long currentTime = millis();
+    if (currentTime - this->lastChangeTime > this->delayTime) {
+        if (this->currentValue < this->targetValue) {
+            this->currentValue++;
+        } else {
+            this->currentValue--;
+        }
+        analogWrite(this->pin, this->currentValue);
+        this->lastChangeTime = currentTime;
+    }
+}
---
+++ src/SmoothPin.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Arduino.h>
+
+class SmoothPin {
+    private:
+        uint8_t pin;
+        int currentValue;
+        int targetValue;
+        unsigned long lastChangeTime;
+        unsigned long delayTime;
+    public:
+        SmoothPin(uint8_t pin);
+        void write(int value);
+        /**
+         * @brief Write value to pin with delay
+         * @param value value to write
+        */
+        void write(int value, unsigned int totalDelayMillis);
+        void update();
+};
