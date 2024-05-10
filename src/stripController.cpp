d749fbe782c342e5d1e3875e1fd6f80792481b62
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-05-10 22:20:38 +0300

    create simple stripController
    a simple wrapper to just update what is going on when disconnected

---
+++ src/stripController.cpp
@@ -0,0 +1,27 @@
+#include "stripController.h"
+
+void stripController::updateLEDStatus(LedStrip* strip, Status status) {
+    switch (status) {
+        case CONNECTED: case SYNCED:
+            // early return as the strip is already set to the connected color
+            return;
+            break;
+        case UNKNOWN:
+            // blink cyan when unknown
+            strip->fillColor(SimpleColor(0, 255, 255));
+            break;
+        case DISCONNECTED:
+            // blink red when disconnected
+            strip->fillColor(SimpleColor(255, 0, 0));
+            break;
+        case OTA:
+            // blink yellow when in OTA mode
+            strip->fillColor(SimpleColor(255, 255, 0));
+            break;
+        case ERROR:
+            // blink orange when in error
+            strip->fillColor(SimpleColor(255, 165, 0));
+            break;
+    }
+    strip->changeModeTo(Modes::Blink);
+}
