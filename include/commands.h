72d85316d7a6efa4bdb33efddca14332a3c48d75
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-06-01 05:46:34 +0300

    Delete commands.h

--- include/commands.h
+++
@@ -1,5 +0,0 @@
-#include "CloudSerial.h" // private library, on lib/CloudSerial
-#include <WiFi.h>
-#include "stringTools.h" // private library, on lib/stringTools
-
-void setupCommands(CloudSerialSystem* cloudSerialSystem);
