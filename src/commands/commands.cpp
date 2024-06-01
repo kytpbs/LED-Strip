cfdf5da48fed01937ddaf20e7dccc0b16c88ada8
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-06-01 06:04:03 +0300

    re-order function calls in setupCommands
    addAllCommands should be in the end, even if it doesn't matter now, it might in the future

--- src/commands/commands.cpp
+++ src/commands/commands.cpp
@@ -1,7 +1,6 @@
 #include "commands/commands.h"
 
 void setupCommands(CloudSerialSystem* cloudSerialSystem, LedStrip* strip) {
-    commandHelpers::addAllCommands(cloudSerialSystem);
     stripCommands::setupCommands(strip);
+    commandHelpers::addAllCommands(cloudSerialSystem); //! should be always at the end
 }
-
